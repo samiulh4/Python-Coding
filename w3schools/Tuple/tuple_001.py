c992cb07dd96cfe85acba2975e7a9b1897664b79
Your Name <you@example.com>
2022-05-02 01:23:18 +0600

    w3school

---
+++ w3schools/Tuple/tuple_001.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    mytuple = ('apple', 'banana', 'cherry')
+    print('Displaying Tuple :')
+    print(mytuple)
+    print('Length Of Tuple :')
+    print(len(mytuple))
+    print('Type Of Tuple :')
+    print(type(mytuple))
